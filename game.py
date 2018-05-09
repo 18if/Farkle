6802ac9a56102dcf2817c782752dfc3f9a458b91
18if <31894573+18if@users.noreply.github.com>
2018-05-09 11:34:26 -0400

    Update game.py
    added dice rolling

--- game.py
+++ game.py
@@ -1,1 +1,15 @@
-
+import random
+class Farkle:
+  
+  
+    def __init__(self):
+      self.gameover = False
+      self.players = int(input('How many players? '))
+      self.playing = range(self.players)
+    def die(self):
+      for i in range(6):
+        roll = random.randint(1,6)
+        print(roll)
+    def play(self):
+      for player in self.playing:
+        self.die
