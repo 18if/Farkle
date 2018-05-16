03a7b8fe5bc414be1a0c52c0f8fc5d337dc677a4
18if <31894573+18if@users.noreply.github.com>
2018-05-16 17:01:00 -0400

    Update game.py
    added scores and questions to ask Mr. Enrico the next time I see him. Brainstormed ideas on how to go about fixing some issues

--- game.py
+++ game.py
@@ -1,5 +1,12 @@
 # wins when has 10,000 points
 # scoring die will be in scoring sheet
+"""
+Questions:
+  mentioning roll twice. not sure if necessary
+  help wiht play and move functions
+  need help cleaning up my code. Specifically play, move, and die
+  how to store roll to use it's values later. pop?
+"""
 import math
 import random
 class Farkle:
@@ -9,30 +16,28 @@ class Farkle:
     self.gameover = False
     self.players = int(input('How many players? '))
     self.playing = range(self.players)
-    self.score = # no idea how to do this
     
-  def scoring_sheet(self):
-# scoring die are:
-# 1	100 points
-# 5	50 points
-# Three 1's	1,000 points
-# Three 2's	200 points
-# Three 3's	300 points
-# Three 4's	400 points
-# Three 5's	500 points
-# Three 6's	600 points
-# 1-2-3-4-5-6 	3000 jnhpoints
-# 3 Pairs	1500 points (including 4-of-a-kind and a pair)
-# ideas: use "math" to add and multiplt the dice
-    pass
   def die(self):
     for i in range(6):
       roll = random.randint(1,6)
-      print(roll)
+      print(roll)    
+      # need to store it somehow in order to use it later to score
+  def scoring_sheet(self):
+    thousand = 1,1,1
+    two_hundred = 2,2,2
+    three_hundred = 3,3,3
+    four_hundred = 4,4,4
+    five_hundred = 5,5,5
+    six_hundred = 6,6,6
+    all_dice = 1,2,3,4,5,6
+    scores = {1:100, 5:50, thousand:1000, two_hundred:200, three_hundred:300,four_hundred:400, five_hundred:500, six_hundred:600, all_dice:3000}
+          roll = random.randint(1,6)
+    my_score = my_score += scores[roll]
+    pass
       
   def move(self):
     for player in self.playing:
-      
+      self.die()
     # player moves
     # player puts aside whatever die they want
     # player moves untill all die are aside or fail to roll scoring die
