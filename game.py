3268a7f25882ba3293a5f2c125b2feee2e0ff4e9
18if <31894573+18if@users.noreply.github.com>
2018-05-27 18:22:09 -0400

    Update game.py

--- game.py
+++ game.py
@@ -6,21 +6,20 @@ class Farkle:
     self.rolls = []
     self.gameover = False
     self.players = int(input('How many players? '))
-    self.playing = range(self.players)
+    self.playing = range(1,self.players)
     self.my_score = 0
-    self.scores = {1:100, 2:200, 3:300, 4:400, 5:500, 6:600}
+    self.scores = {1:100, 2:20, 3:30, 4:40, 5:50, 6:60}
 
   def move(self):
     for i in range(5):
       self.rolls.append(random.randint(1,6))
       self.my_score += sum(self.scores[a] for a in self.rolls)
   def play(self):
-    while self.gameover:
+    while not self.gameover:
       for player in self.playing:
         self.move()
         print(self.my_score)
         if self.my_score >= 10000:
-          self.gameover = True
           print("Player {} wins!".format(player))
+          self.gameover = True
           break
-a = Farkle()
