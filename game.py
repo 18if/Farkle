0f566cd9d8b681b242a837e291b5bce4207ddb01
18if <31894573+18if@users.noreply.github.com>
2018-06-09 00:34:25 -0400

    Update game.py

--- game.py
+++ game.py
@@ -5,7 +5,7 @@ class Farkle:
   
   def __init__(self, playing = 2):
     self.SCORES = {
-      1: {1: 100, 2: 200, 3: 1000, 4: 2000, 5: 2100, 6: 2200},
+      1: {1: 100, 2: 200, 3: 300, 4: 2000, 5: 2100, 6: 2200},
       2: {1: 0, 2: 0, 3: 200, 4: 400, 5: 500, 6: 600},
       3: {1: 30, 2: 60, 3: 300, 4: 600, 5: 1000, 6: 2000},
       4: {1: 40, 2: 80, 3: 400, 4: 800, 5: 2000, 6: 2500},
@@ -16,7 +16,26 @@ class Farkle:
   def move(self, player):
     self.rolls = [random.randint(1,6) for n in range(6)]
     print('Player {} Rolled: {}'.format(player+1, self.rolls))
-
+    while True:
+      indexes = input("Pick the indices you wish to extract separated by spaces(remember to start from 0)").split(' ')
+      indexes = [int(n) for n in indexes]
+      self.take_from_list(player, self.rolls, indexes)
+      question = str(input('Roll again? (y/n)'))
+      if question == 'y':
+        die_num = self.new_list[0]
+        c = Counter(self.new_list)
+        b = c[die_num]
+        die_score = self.SCORES[die_num][b]
+        self.scores[player] += die_score
+        print("Score last move: {}".format(die_score))    
+        self.rolls = [random.randint(1,6) for n in self.remaining]
+        print('Player {} Rolled: {}'.format(player+1, self.rolls))
+        if self.rolls == []:
+          self.rolls = [random.randint(1,6) for n in range(6)]
+      if question == 'n':
+        print('ok')
+        return False
+  
   def take_from_list(self, player, source, indexes):
     """ Given a source list, create a new list from a list of indexes, returning the new list and what remains from the old list 
     
@@ -34,39 +53,14 @@ class Farkle:
         self.remaining.append(val)
 
     print(self.new_list, self.remaining)
-  
-  def complicated_scores(self, rolls, die_num, player):
-    c = Counter(self.new_list)
-    b = c[die_num]
-    die_score = self.SCORES[die_num][b]
-    self.scores[player] += die_score
-    print("Score this turn: {}".format(die_score))
 
-  def reroll(self, player):
-    while True:
-      indexes = input("Pick the indices you wish to extract separated by spaces(remember to start from 0)").split(' ')
-      indexes = [int(n) for n in indexes]
-      self.take_from_list(player, self.rolls, indexes)
-      question = str(input('Roll again? (y/n)'))
-      if question == 'y':    
-        rolls = [random.randint(1,6) for n in self.remaining]
-        print('Player {} Rolled: {}'.format(player+1, rolls))
-      if rolls == []:
-        return False
-
-      if question == 'n':
-        print('ok')
-        return False
       
 
 
 
   def play(self, player):
     self.move(player)
-    die_num = self.new_list[0]
-    self.complicated_scores(self.rolls, die_num, player)
     print(self.scores)
-    self.reroll(player)
     
 
 class Die:
